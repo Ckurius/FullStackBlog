707704999857646391406a89a3af95dd823c774c
yongqi8899 <1243994415@qq.com>
2024-07-24 07:12:29 +0200

    add CRUD

--- index.js
+++ index.js
@@ -1,40 +1,86 @@
-import express from 'express'; // Import express
-import cors from 'cors';
-import pg from 'pg';
+import express from "express"; // Import express
+import cors from "cors";
+import pg from "pg";
+const { Pool } = pg;
+
+import bodyParser from "body-parser";
 
 /* Need to run npm i , npm i cors , npm i express , npm i pg , npm i -D dotenv   */
 
-const PORT = process.env.PORT || 3000; // Declare a port variable
+const PORT = process.env.PORT || 8000; 
 
-const app = express(); // Creat your app object
+const app = express(); 
+const pool = new Pool({ connectionString: process.env.PG_URI });
 
 app.use(express.json());
 app.use(cors());
-
-const posts = [
-  { id: 1, title: 'Post 1', content: 'testcontent1', cover: 'img1' },
-  { id: 2, title: 'Post 2', content: 'testcontent2', cover: 'img2' },
-  { id: 3, title: 'Post 3', content: 'testcontent3', cover: 'img3' },
-]; // Simple array to represent data
-
-app.get('/posts', (req, res) =>
-  res.json({ message: 'Retrieve all posts', posts })
-);
-
-app.get('/posts/:id', (req, res) => {
-  res.json({ message: 'Retrieve a single post by ID' });
+app.use(bodyParser.json());
+// get all posts
+app.get("/posts", async (req, res) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM posts");
+    return res.status(200).json(rows);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 });
-
-app.post('/posts', (req, res) => {
-  res.json({ message: 'Create a new post' });
+// get a single post
+app.get("/posts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { rows } = await pool.query("SELECT * FROM posts WHERE id = $1", [
+      id,
+    ]);
+    if (!rows.length) throw new Error("Order not found");
+    return res.json(rows);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 });
+// create a post
+app.post("/posts", async (req, res) => {
+  try {
+    const { title, author, content, cover } = req.body;
+    if (!title || !author || !content || !cover)
+      throw new Error("Missing data");
 
-app.put('/posts/:id', (req, res) =>
-  res.json({ message: 'Update an existing post by id' })
-);
+    const results = await pool.query(
+      "INSERT INTO posts (title, author, content, cover) VALUES ($1, $2, $3, $4) RETURNING *",
+      [title, author, content, cover]
+    );
+    const posts = results.rows[0];
+    return res.json(posts);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+// update a post
+app.put("/posts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, author, content, cover } = req.body;
+    if (!title || !author || !content) throw new Error("Missing data");
 
-app.delete('/posts/:id', (req, res) =>
-  res.json({ message: 'DELETE a post by id' })
-);
+    const { rows } = await pool.query(
+      "UPDATE posts SET title = $1, author = $2, content = $3, cover = $4 WHERE id = $5 RETURNING *;",
+      [title, author, content, cover, id]
+    );
+
+    return res.json(rows);
+  } catch (error) {
+    console.error("Fehler beim Aktualisieren des Posts", error.stack);
+    return res.status(500).json({ error: "Interner Serverfehler" });
+  }
+});
+// delete a post
+app.delete("/posts/:id", async (req, res) => {
+  try {
+    const {id} = req.params;
+    await pool.query('DELETE FROM posts WHERE id = $1 RETURNING *', [id]);
+    return res.json({ success: 'Posts deleted' });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
