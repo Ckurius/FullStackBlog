62707e20bb45b80cf19a220205957838376102f0
Fon Yingrak Muansawad <mynameisfonja@gmail.com>
2024-07-24 15:52:22 +0200

    GET (#2)
    Co-authored-by: Ckurius <chkuri@chkuri.de>

--- index.js
+++ index.js
@@ -14,20 +14,46 @@ const pool = new Pool({ connectionString: process.env.PG_URI });
 
 app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json());
-const posts = [
-  { id: 1, title: 'Post 1', content: 'testcontent1', cover: 'img1' },
-  { id: 2, title: 'Post 2', content: 'testcontent2', cover: 'img2' },
-  { id: 3, title: 'Post 3', content: 'testcontent3', cover: 'img3' },
-]; // Simple array to represent data
 
-app.get('/posts', (req, res) =>
-  res.json({ message: 'Retrieve all posts', posts })
-);
 
-app.get('/posts/:id', (req, res) => {
-  res.json({ message: 'Retrieve a single post by ID' });
+// Create a SQL pool object to connect to the database
+const pool = new pg.Pool({
+  connectionString: process.env.PG_URI,
 });
+
+
+// Define a GET route to fetch data from the database
+app.get('/posts', async (req, res) => {
+  try {
+    const client = await pool.connect();                    // client will be the connection to the database
+    const result = await pool.query('SELECT * FROM posts'); // this is the query to the database
+    const posts = result.rows;                              // This is the result of the query returned from the database in rows
+    client.release();                                       // this means that the connection is released /ended
+    res.json(posts);                                        //  res is the response to the client and we are sending the posts
+  } catch (err) {
+    console.error('Error executing query', err.stack);
+    res.status(500).send('Error connecting to database');
+  }
+});
+
+// Define a GET route to fetch a single post by ID
+app.get('/posts/:id', async (req, res) => {
+  const { id } = req.params;                                      // this is the id that is passed in the url
+  try {
+    const client = await pool.connect();
+    const result = await pool.query('SELECT * FROM posts WHERE id = $1', [id]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Post not found' });
+    }
+
+    res.status(200).json(result.rows[0]);
+    client.release();
+  } catch (err) {
+    console.error('Error executing query', err.stack);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 
 app.post('/posts', (req, res) => {
   res.json({ message: 'Create a new post' });
